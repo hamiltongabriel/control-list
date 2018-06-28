2d0c47e85b4eb0d16964cf2372f571510b1b2c75
unknown <gabrielzinho.gabriel2@gmail.com>
2018-06-28 14:26:57 -0300

    Start Project Review

--- shopping/js/main.js
+++ shopping/js/main.js
@@ -4,7 +4,7 @@ let list = [
     { "descricao": "meat", "quantity": "1", "value": "15.00" }
 ];
 
-getTotal = (list) =>{
+function getTotal(list){
     var total = 0;
     for (let key in list){
         total += list[key].value * list[key].quantity;
@@ -12,7 +12,7 @@ getTotal = (list) =>{
     return total;
 }
 
-setList = (list) =>{
+function setList(list){
     let table = '<thead><tr><td>Description</td><td>Quantity</td><td>Value</td><td>Action</td></tr></thead><tbody>';
     for(let key in list){
         table += `<tr><td>${list[key].descricao}</td><td>${list[key].quantity}</td><td>${list[key].value}</td><td>edit | delete</td></tr>`
