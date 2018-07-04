d733fd3c5d1b8488d4b65c467d6f814674b75c5b
unknown <gabrielzinho.gabriel2@gmail.com>
2018-07-04 13:55:53 -0300

    Implementando Campo para inserir novos atributos.

--- shopping/js/main.js
+++ shopping/js/main.js
@@ -12,12 +12,36 @@ function getTotal(list){
     return total;
 }
 
+
 function setList(list){
     let table = '<thead><tr><td>Description</td><td>Quantity</td><td>Value</td><td>Action</td></tr></thead><tbody>';
     for(let key in list){
-        table += `<tr><td>${list[key].descricao}</td><td>${list[key].quantity}</td><td>${list[key].value}</td><td>edit | delete</td></tr>`
+        table += `<tr><td>${ formatDescricao(list[key].descricao)}</td><td>${list[key].quantity}</td><td>${formatValue(list[key].value)}</td><td>edit | delete</td></tr>`
     }
     table += '<tbody/>';
     document.getElementById('listTable').innerHTML = table;
 };
+
+function formatDescricao(descricao){
+    var str = descricao.toLowerCase();
+    str = `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+    return str;
+}
+
+function formatValue(value){
+    debugger
+    var str = `${parseFloat(value).toFixed(2)}`;
+    str = str.replace(".",",");
+    str = `$ ${str}`
+    return str;
+}
+
+function addData(){
+    var descricao = document.getElementById("descricao").value;
+    var quantity = document.getElementById("quantity").value;
+    var value = document.getElementById("value").value;
+
+    list.unshift({"descricao": descricao, "quantity": quantity, "value": value});
+    setList(list);
+}
 setList(list);
