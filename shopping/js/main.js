978a3384dc0268fcd270f28e4202254bfbfe0228
unknown <gabrielzinho.gabriel2@gmail.com>
2018-07-05 13:31:36 -0300

    Atualizando Dados

--- shopping/js/main.js
+++ shopping/js/main.js
@@ -4,48 +4,48 @@ let list = [
     { "descricao": "meat", "quantity": "1", "value": "15.00" }
 ];
 
-function getTotal(list){
+function getTotal(list) {
     var total = 0;
-    for (let key in list){
+    for (let key in list) {
         total += list[key].value * list[key].quantity;
     }
     return total;
 }
 
 
-function setList(list){
+function setList(list) {
     let table = '<thead><tr><td>Description</td><td>Quantity</td><td>Value</td><td>Action</td></tr></thead><tbody>';
-    for(let key in list){
-        table += `<tr><td>${ formatDescricao(list[key].descricao)}</td><td>${list[key].quantity}</td><td>${formatValue(list[key].value)}</td><td><button class="btn btn-success" onclick="setUpdate(${key});">Edit</button> | <button class="btn btn-danger">delete</button></td></tr>`
+    for (let key in list) {
+        table += `<tr><td>${formatDescricao(list[key].descricao)}</td><td>${list[key].quantity}</td><td>${formatValue(list[key].value)}</td><td><button class="btn btn-success" onclick="setUpdate(${key});">Edit</button> | <button class="btn btn-danger">delete</button></td></tr>`
     }
     table += '<tbody/>';
     document.getElementById('listTable').innerHTML = table;
 };
 
-function formatDescricao(descricao){
+function formatDescricao(descricao) {
     var str = descricao.toLowerCase();
     str = `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
     return str;
 }
 
-function formatValue(value){
+function formatValue(value) {
     debugger
     var str = `${parseFloat(value).toFixed(2)}`;
-    str = str.replace(".",",");
+    str = str.replace(".", ",");
     str = `$ ${str}`
     return str;
 }
 
-function addData(){
+function addData() {
     let descricao = document.getElementById("descricao").value;
     let quantity = document.getElementById("quantity").value;
     let value = document.getElementById("value").value;
 
-    list.unshift({"descricao": descricao, "quantity": quantity, "value": value});
+    list.unshift({ "descricao": descricao, "quantity": quantity, "value": value });
     setList(list);
 }
 
-function setUpdate(id){
+function setUpdate(id) {
     var obj = list[id];
     document.getElementById("descricao").value = obj.descricao;
     document.getElementById("quantity").value = obj.quantity;
@@ -55,11 +55,23 @@ function setUpdate(id){
     document.getElementById("inputIdUpdate").innerHTML = `<input id="idUpdate" type="hidden" value="${id}"/>`
 }
 
-function cancelData(){
+function cancelData() {
     document.getElementById("descricao").value = "";
     document.getElementById("quantity").value = "";
     document.getElementById("value").value = "";
     document.getElementById("btnUpdate").style.display = "none";
     document.getElementById("btnAdd").style.display = "inline-block";
+    document.getElementById("inputIdUpdate").innerHTML = "";
+}
+
+function saveData() {
+    let id = document.getElementById("idUpdate").value;
+    let descricao = document.getElementById("descricao").value;
+    let quantity = document.getElementById("quantity").value;
+    let value = document.getElementById("value").value;
+
+    list[id] = { "descricao": descricao, "quantity": quantity, "value": value };
+    cancelData();
+    setList(list);
 }
 setList(list);
