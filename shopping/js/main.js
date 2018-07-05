507b354c3f13390b93811aec2891bc2a4c69c858
unknown <gabrielzinho.gabriel2@gmail.com>
2018-07-05 13:24:47 -0300

    Preparando para atualizacao de registros

--- shopping/js/main.js
+++ shopping/js/main.js
@@ -16,7 +16,7 @@ function getTotal(list){
 function setList(list){
     let table = '<thead><tr><td>Description</td><td>Quantity</td><td>Value</td><td>Action</td></tr></thead><tbody>';
     for(let key in list){
-        table += `<tr><td>${ formatDescricao(list[key].descricao)}</td><td>${list[key].quantity}</td><td>${formatValue(list[key].value)}</td><td>edit | delete</td></tr>`
+        table += `<tr><td>${ formatDescricao(list[key].descricao)}</td><td>${list[key].quantity}</td><td>${formatValue(list[key].value)}</td><td><button class="btn btn-success" onclick="setUpdate(${key});">Edit</button> | <button class="btn btn-danger">delete</button></td></tr>`
     }
     table += '<tbody/>';
     document.getElementById('listTable').innerHTML = table;
@@ -37,11 +37,29 @@ function formatValue(value){
 }
 
 function addData(){
-    var descricao = document.getElementById("descricao").value;
-    var quantity = document.getElementById("quantity").value;
-    var value = document.getElementById("value").value;
+    let descricao = document.getElementById("descricao").value;
+    let quantity = document.getElementById("quantity").value;
+    let value = document.getElementById("value").value;
 
     list.unshift({"descricao": descricao, "quantity": quantity, "value": value});
     setList(list);
+}
+
+function setUpdate(id){
+    var obj = list[id];
+    document.getElementById("descricao").value = obj.descricao;
+    document.getElementById("quantity").value = obj.quantity;
+    document.getElementById("value").value = obj.value;
+    document.getElementById("btnUpdate").style.display = "inline-block";
+    document.getElementById("btnAdd").style.display = "none";
+    document.getElementById("inputIdUpdate").innerHTML = `<input id="idUpdate" type="hidden" value="${id}"/>`
+}
+
+function cancelData(){
+    document.getElementById("descricao").value = "";
+    document.getElementById("quantity").value = "";
+    document.getElementById("value").value = "";
+    document.getElementById("btnUpdate").style.display = "none";
+    document.getElementById("btnAdd").style.display = "inline-block";
 }
 setList(list);
